dfa18beba168866787bdf2b8d0f1b813cb7a9505
Denis <denismunene999@gmail.com>
2022-10-13 14:51:15 +0300

    style article component

--- client/src/components/home/Article.js
+++ client/src/components/home/Article.js
@@ -10,7 +10,16 @@ function Article({article}) {
       <hr className='article-hr' />
       <div className='profile'>
         <img className='profile_image' src={user.profile_image} alt='DP' />
-        <p>{user.username}</p>
+        <h4 className='username'>{user.username}</h4>
+        <p className='date_created'>• {created_at.substring(0, 10)}</p>
+      </div>
+      <div className='content'>
+        <h3 className='title'>{title}</h3>
+        <p className='story'>{story.substring(0, 300)}...</p>
+      </div>
+      <div className='btns'>
+        <button className='btn'>{topic}</button>
+        <button className='btn'>{time} minutes read</button>
       </div>
     </div>
   )
